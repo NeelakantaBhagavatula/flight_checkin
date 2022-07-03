b5123010b6d01e507d1c493f9ef98e5391e43943
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-07-02 20:07:21 -0700

    Implemented feature for flight check-in using flight reservation REST API

---
+++ flightcheckin/src/main/java/sjsu/edu/flightcheckin/integration/ReservationRestClient.java
@@ -0,0 +1,12 @@
+ package sjsu.edu.flightcheckin.integration;
+
+import sjsu.edu.flightcheckin.integration.dto.Reservation;
+import sjsu.edu.flightcheckin.integration.dto.ReservationUpdateRequest;
+
+public interface ReservationRestClient {
+
+	public Reservation findReservation(Long id);
+	
+	public Reservation updateReservation(ReservationUpdateRequest request);
+	
+}
